68a8ddf2f3eb6cc26c5bc25cee4f316d9d74e2e2
marinafischer <marinamirandafischer@gmail.com>
2022-05-23 15:21:03 -0300

    fix: postCategoryModel keys

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,21 +1,29 @@
 const PostCategory = (sequelize, DataTypes) => {
-  const PostCategory = sequelize.define("PostCategory", 
-  {},
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+  },
   {
     timestamps: false,
   });
   PostCategory.associate = (models)=>{
+    models.Category.belongsToMany(models.BlogPost,{
+      as: 'posts',
+      through: PostCategory,
+      foreignKey:'categoryId',
+      otherKey:'postId',
+    });
     models.BlogPost.belongsToMany(models.Category,{
       as: 'categories',
       through: PostCategory,
       foreignKey:'postId',
       otherKey:'categoryId'
-    });
-    models.Category.belongsToMany(models.BlogPost,{
-      as: 'posts',
-      through: PostCategory,
-      foreignKey:'categoryId',
-      otherKey:'postId'
     });
   }
 
